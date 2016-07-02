7609100d099fdcab78938ba63e582398301019f0
arundhatikurup <arundhatikurup@yahoo.com>
2016-07-02 17:04:30 +0530

    during temp Commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
+
 from django.shortcuts import render
 
+from .models import Post
+
 # Create your views here.
+
+def post_list(request):    
+    d = {
+     'posts': Post.objects.all(),
+    }
+
+
+
+    return render(request, 'post_list.html', d)
